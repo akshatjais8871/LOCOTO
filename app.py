50228b0307f1556f826171c0717bbdb965e41717
akshatjais8871 <akshatjaiswal017@gmail.com>
2025-03-11 14:15:47 +0530

    ..

--- app.py
+++ app.py
@@ -1,11 +1,75 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import bcrypt
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'secret'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+    
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+    
+with app.app_context():
+    db.create_all()
+
+
+    
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first() or User.query.filter_by(email=username).first()
+        if user and user.check_password(password):
+            return redirect(url_for('home'))
+        return render_template('login.html', error='Invalid username or password')
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        conform_password = request.form['confirm-password']
+        if password != conform_password:
+            return render_template('register.html', error='Passwords do not match')
+        try:
+            if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+                return render_template('register.html', error='Username already exists')
+        except:
+            pass
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
